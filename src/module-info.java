a15e7fefa7187165f432e2ae4289a6a8a24895f4
stemorizinho <estevaoroyal14@gmail.com>
2022-10-09 11:52:14 -0300

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Barcellos
+ *
+ */
+module primeiroprograma {
+}
---
+++ src/primeiroprograma/primeiroprograma.java
@@ -0,0 +1,27 @@
+package primeiroprograma;
+
+import java.util.Objects; 
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		gato Gato = new gato();
+	   Livros livros = new Livros(); 
+				
+        System.out.println(Gato);
+        System.out.println(livros);
+	
+
+}
+
+	
+	
+	
+	
+	
+class Livros {
+	
+	private String nome;
+	private String npag;
+}
